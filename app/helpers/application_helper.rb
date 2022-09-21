fee52f9c83dbec221a0a8aa86f06811f94cb9a11
Smooke09 <pedrolucas1014zz@gmail.com>
2022-09-21 14:49:09 -0300

    identifying the environments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     def name_application
         "CRYPTO WALLET APP"
     end
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production
+              "Produção"
+        else 
+            "Teste"
+        end
+    end
 end
